95ae8f64a7fcff501a085753ca0c9c885e11a288
Faisal Haque Bappy <hbfaisal66@gmail.com>
2019-10-15 04:49:56 +0600

    API initiated

---
+++ ChatServer/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  name: String,
+  id: String,
+  photo: String,
+  isActive: {
+    type: Boolean,
+    default: false
+  }
+});
+
+module.exports = mongoose.model("Users", UserSchema);
