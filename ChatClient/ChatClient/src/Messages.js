0e1b0e12c1cf6a2f21164f758ebd03c6b9c924d7
Faisal Haque Bappy <hbfaisal66@gmail.com>
2019-10-16 11:36:22 +0600

    Chat Screen modified

--- ChatClient/ChatClient/src/Messages.js
+++ ChatClient/ChatClient/src/Messages.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {GiftedChat, Bubble} from 'react-native-gifted-chat';
+import {View, StatusBar} from 'react-native';
+import {Header} from 'react-native-elements';
+import {GiftedChat, Bubble, InputToolbar, Send} from 'react-native-gifted-chat';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class Messages extends React.Component {
   constructor(props) {
@@ -16,15 +19,48 @@ class Messages extends React.Component {
         {...props}
         textStyle={{
           right: {
-            color: 'yellow',
+            color: '#fff',
           },
         }}
         wrapperStyle={{
           right: {
-            backgroundColor: 'red',
+            backgroundColor: '#46CF76',
           },
         }}
       />
+    );
+  };
+
+  renderInputToolbar = props => {
+    return (
+      <InputToolbar
+        {...props}
+        containerStyle={{
+          backgroundColor: '#111',
+          borderTopWidth: 0,
+          marginHorizontal: 10,
+          borderRadius: 80,
+        }}
+        textStyle={{
+          color: '#fff',
+        }}
+      />
+    );
+  };
+
+  renderSend = props => {
+    return (
+      <Send {...props}>
+        <Icon
+          name="ios-arrow-dropright-circle"
+          style={{
+            color: '#46CF76',
+            marginRight: '0%',
+            marginBottom: '20%',
+          }}
+          size={32}
+        />
+      </Send>
     );
   };
 
@@ -53,15 +89,41 @@ class Messages extends React.Component {
 
   render() {
     return (
-      <GiftedChat
-        style={{backgroundColor: '#222'}}
-        messages={this.state.messages}
-        renderBubble={this.renderBubble}
-        onSend={messages => this.onSend(messages)}
-        user={{
-          _id: 1,
-        }}
-      />
+      <>
+        <StatusBar backgroundColor="#111" barStyle="light-content" />
+
+        <View style={{backgroundColor: '#222', flex: 1}}>
+          <View
+            style={{
+              height: '8%',
+              width: '100%',
+              backgroundColor: '#111',
+            }}></View>
+          <GiftedChat
+            listViewProps={{
+              style: {
+                backgroundColor: '#222',
+              },
+            }}
+            renderUsernameOnMessage={true}
+            isAnimated={true}
+            alwaysShowSend={true}
+            messages={this.state.messages}
+            renderBubble={this.renderBubble}
+            renderInputToolbar={this.renderInputToolbar}
+            renderSend={this.renderSend}
+            onSend={messages => this.onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
+          <View
+            style={{
+              height: '1%',
+              width: '100%',
+            }}></View>
+        </View>
+      </>
     );
   }
 }
